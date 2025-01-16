69aa167a7212e33b611221b856cef807811526af
Conner Krenzer <krenzer84@gmail.com>
2025-01-16 11:53:35 -0500

    wip

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const server = express();
+const recipesRouter = require("./recipes/recipes-router");
+
+server.use(express.json());
+
+server.use("/api/recipes", recipesRouter);
+
+server.use("*", (req, res) => {
+  res.json({ message: "the API is running! go catch it!" });
+});
+server.use((error, req, res, next) => {
+  res.status(500).json({ message: "internal server error! uh oh!" });
+});
+
+module.exports = server;
